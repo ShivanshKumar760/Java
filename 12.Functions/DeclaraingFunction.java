b06021d66662aca0a1784e0c03eb94e1c4efad79
Shivansh <shivanshkumar760@gmail.com>
2023-06-09 22:35:33 +0530

    Third Commit

---
+++ 12.Functions/DeclaraingFunction.java
@@ -0,0 +1,12 @@
+public class DeclaraingFunction
+{
+	public static void PrintHello()
+	{
+		System.out.println("Hello This is a function generated message");
+	}
+
+	public static void main(String[] args)
+	{
+		PrintHello();//Function call
+	}
+}
---
+++ 12.Functions/FunctionWithReturnType.java
@@ -0,0 +1,7 @@
+public class FunctionWithReturnType
+{
+	public static int Sum(int a,int b){return a+b;}
+
+	public static void main(String[] args){int a =3; int b=4; System.out.println(Sum(a,b));}
+
+}
