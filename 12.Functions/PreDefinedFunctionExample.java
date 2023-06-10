1b6d61c4e1822c3adc7de4f04844716a72ef1308
Shivansh <shivanshkumar760@gmail.com>
2023-06-10 12:12:47 +0530

    Fourth Commit

---
+++ 12.Functions/PreDefinedFunctionExample.java
@@ -0,0 +1,11 @@
+//Example of pre defined function 
+public class PreDefinedFunctionExample
+{
+	public static void main(String[] args)
+	{
+		System.out./*print() is a predefined function in  class PrintStream */print("The max is:"+Math.max(1,3));
+		/*the max() method falls under Math class that returns the greater of two number .*/
+		
+
+	}
+}
